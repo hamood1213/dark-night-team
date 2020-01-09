6b160a928119eebcaec16ccc0c94f1856e977d91
hamood1213 <59527022+hamood1213@users.noreply.github.com>
2020-01-10 00:07:56 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -11,8 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$bc |dark night`,"...
- Shop")
+client.user.setGame(`$bc |DF_Team`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
